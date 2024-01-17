183f7366e21b9e7d5c4fe2c2256ddce9b8ae5f7d
Fiantso Harena <fiantsorav@gmail.com>
2024-01-17 17:07:46 +0300

    feat: create a new config file to store defaultSettings

---
+++ src/config/Config.ts
@@ -0,0 +1,16 @@
+export const defaultSettings: {
+    themeMode: string;
+    themeDirection: string;
+    themeContrast: string;
+    themeLayout: string;
+    themeColorPresets: string;
+    themeStretch: boolean;
+  } = {
+    themeMode: "light",
+    themeDirection: "ltr",
+    themeContrast: "default",
+    themeLayout: "horizontal",
+    themeColorPresets: "default",
+    themeStretch: false,
+  };
+  
--- src/context/SettingsContext.ts
+++ src/context/SettingsContext.ts
@@ -1,3 +1,187 @@
-const SettingsContext: String = "test";
+import React, { createContext, useEffect, FC } from "react";
+import { defaultSettings } from "../config";
+import useLocalStorage from "../hooks/useLocalStorage";
+import getColorPresets, { defaultPreset, colorPresets } from "../utils/getColorPresets";
+
+interface SettingsState {
+  themeMode: string;
+  themeLayout: string;
+  themeStretch: boolean;
+  themeContrast: string;
+  themeDirection: string;
+  themeColorPresets: string;
+}
+
+interface SettingsContextProps extends SettingsState {
+  onToggleMode: () => void;
+  onChangeMode: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  onToggleDirection: () => void;
+  onChangeDirection: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  onChangeDirectionByLang: (lang: string) => void;
+  onToggleLayout: () => void;
+  onChangeLayout: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  onToggleContrast: () => void;
+  onChangeContrast: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  onChangeColor: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  onToggleStretch: () => void;
+  onResetSetting: () => void;
+  setColor: (preset: string) => void;
+  colorOption: { name: string; value: string }[];
+}
+
+const initialState: SettingsContextProps = {
+  ...defaultSettings,
+  onToggleMode: () => {},
+  onChangeMode: () => {},
+  onToggleDirection: () => {},
+  onChangeDirection: () => {},
+  onChangeDirectionByLang: () => {},
+  onToggleLayout: () => {},
+  onChangeLayout: () => {},
+  onToggleContrast: () => {},
+  onChangeContrast: () => {},
+  onChangeColor: () => {},
+  onToggleStretch: () => {},
+  onResetSetting: () => {},
+  setColor: () => {},
+  colorOption: [],
+};
+
+const SettingsContext = createContext<SettingsContextProps>(initialState);
+
+const SettingsProvider: FC = ({ children }) => {
+  const [settings, setSettings] = useLocalStorage<SettingsState>("settings", {
+    themeMode: initialState.themeMode,
+    themeLayout: initialState.themeLayout,
+    themeStretch: initialState.themeStretch,
+    themeContrast: initialState.themeContrast,
+    themeDirection: initialState.themeDirection,
+    themeColorPresets: initialState.themeColorPresets,
+  });
+
+  const isArabic = localStorage.getItem("i18nextLng") === "ar";
+
+  useEffect(() => {
+    if (isArabic) {
+      onChangeDirectionByLang("ar");
+    }
+  }, [isArabic]);
+
+  const onToggleMode = () => {
+    setSettings({
+      ...settings,
+      themeMode: settings.themeMode === "light" ? "dark" : "light",
+    });
+  };
+
+  const onChangeMode = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSettings({
+      ...settings,
+      themeMode: event.target.value,
+    });
+  };
+
+  const onToggleDirection = () => {
+    setSettings({
+      ...settings,
+      themeDirection: settings.themeDirection === "rtl" ? "ltr" : "rtl",
+    });
+  };
+
+  const onChangeDirection = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSettings({
+      ...settings,
+      themeDirection: event.target.value,
+    });
+  };
+
+  const onChangeDirectionByLang = (lang: string) => {
+    setSettings({
+      ...settings,
+      themeDirection: lang === "ar" ? "rtl" : "ltr",
+    });
+  };
+
+  const onToggleLayout = () => {
+    setSettings({
+      ...settings,
+      themeLayout: settings.themeLayout === "vertical" ? "horizontal" : "vertical",
+    });
+  };
+
+  const onChangeLayout = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSettings({
+      ...settings,
+      themeLayout: event.target.value,
+    });
+  };
+
+  const onToggleContrast = () => {
+    setSettings({
+      ...settings,
+      themeContrast: settings.themeContrast === "default" ? "bold" : "default",
+    });
+  };
+
+  const onChangeContrast = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSettings({
+      ...settings,
+      themeContrast: event.target.value,
+    });
+  };
+
+  const onChangeColor = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSettings({
+      ...settings,
+      themeColorPresets: event.target.value,
+    });
+  };
+
+  const onToggleStretch = () => {
+    setSettings({
+      ...settings,
+      themeStretch: !settings.themeStretch,
+    });
+  };
+
+  const onResetSetting = () => {
+    setSettings({
+      themeMode: initialState.themeMode,
+      themeLayout: initialState.themeLayout,
+      themeStretch: initialState.themeStretch,
+      themeContrast: initialState.themeContrast,
+      themeDirection: initialState.themeDirection,
+      themeColorPresets: initialState.themeColorPresets,
+    });
+  };
+
+  return (
+    <SettingsContext.Provider
+      value={{
+        ...settings,
+        onToggleMode,
+        onChangeMode,
+        onToggleDirection,
+        onChangeDirection,
+        onChangeDirectionByLang,
+        onToggleLayout,
+        onChangeLayout,
+        onToggleContrast,
+        onChangeContrast,
+        onChangeColor,
+        onToggleStretch,
+        onResetSetting,
+        setColor: getColorPresets(settings.themeColorPresets),
+        colorOption: colorPresets.map((color) => ({
+          name: color.name,
+          value: color.main,
+        })),
+      }}
+    >
+      {children}
+    </SettingsContext.Provider>
+  );
+};
 
 export { SettingsContext };
+export default SettingsProvider;
