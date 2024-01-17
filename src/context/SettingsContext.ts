f7b8ad3d2bbec8f850194d9e3871c2db9ac2aca9
Fiantso Harena <fiantsorav@gmail.com>
2024-01-17 16:58:30 +0300

    feat: create a new custom hook

---
+++ src/context/SettingsContext.ts
@@ -0,0 +1,3 @@
+const SettingsContext: String = "test";
+
+export { SettingsContext };
---
+++ src/theme/ThemeProvider.tsx
@@ -0,0 +1,43 @@
+import { ReactNode, useMemo } from "react";
+import { CssBaseline } from "@mui/material";
+import {
+  createTheme,
+  ThemeProvider as MUIThemeProvider,
+  StyledEngineProvider,
+  Theme,
+} from "@mui/material/styles";
+import useSettings from "../hooks/useSettings";
+
+interface ThemeProviderProps {
+  children: ReactNode;
+}
+
+export default function ThemeProvider({ children }: ThemeProviderProps) {
+  const { themeMode, themeDirection } = useSettings();
+
+  const isLight = themeMode === "light";
+
+  const themeOptions = useMemo(
+    () => ({
+      palette: isLight ? palette.light : palette.dark,
+      typography,
+      breakpoints,
+      shape: { borderRadius: 8 },
+      direction: themeDirection,
+      shadows: isLight ? shadows.light : shadows.dark,
+      customShadows: isLight ? customShadows.light : customShadows.dark,
+    }),
+    [isLight, themeDirection]
+  );
+
+  const theme: Theme = createTheme(themeOptions);
+
+  return (
+    <StyledEngineProvider injectFirst>
+      <MUIThemeProvider theme={theme}>
+        <CssBaseline />
+        {children}
+      </MUIThemeProvider>
+    </StyledEngineProvider>
+  );
+}
