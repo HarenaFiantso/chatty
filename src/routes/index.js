736e5cd398526b4f19585866b2c8286229cb6fa3
Fiantso Harena <fiantsorav@gmail.com>
2024-01-18 14:13:59 +0300

    refactor: re-implement the router component

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,14 +1,14 @@
+import { Suspense, lazy } from "react";
 import { Navigate, useRoutes } from "react-router-dom";
 
-import DashboardLayout from "../layouts/dashboard/DashboardLayout";
+import DashboardLayout from "../layouts/dashboard";
 
-import MainApp from "../pages/dashboard/MainApp";
-
-import Page404 from "../pages/Page404";
+import { DEFAULT_PATH } from "../config";
+import LoadingScreen from "../components/LoadingScreen";
 
 const Loadable = (Component) => (props) => {
   return (
-    <Suspense fallback="Loading..">
+    <Suspense fallback={<LoadingScreen />}>
       <Component {...props} />
     </Suspense>
   );
@@ -20,18 +20,25 @@ export default function Router() {
       path: "/",
       element: <DashboardLayout />,
       children: [
-        { element: <Navigate to="/app" replace />, index: true },
-        { path: "app", element: <MainApp /> },
-
+        { element: <Navigate to={DEFAULT_PATH} replace />, index: true },
+        { path: "app", element: <GeneralApp /> },
+        { path: "conversation", element: <Conversation /> },
+        { path: "chats", element: <Chats /> },
+        { path: "contact", element: <Contact /> },
         { path: "404", element: <Page404 /> },
         { path: "*", element: <Navigate to="/404" replace /> },
       ],
     },
-
     { path: "*", element: <Navigate to="/404" replace /> },
   ]);
 }
 
-const MainApp = Loadable(
-  lazy(() => import("../pages/dashboard/MainApp"))
+const GeneralApp = Loadable(
+  lazy(() => import("../pages/dashboard/GeneralApp"))
 );
+const Conversation = Loadable(
+  lazy(() => import("../pages/dashboard/Conversation"))
+);
+const Chats = Loadable(lazy(() => import("../pages/dashboard/Chats")));
+const Contact = Loadable(lazy(() => import("../pages/dashboard/Contact")));
+const Page404 = Loadable(lazy(() => import("../pages/Page404")));
