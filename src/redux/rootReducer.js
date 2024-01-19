c79ed773e5dd8b486d9b505fd4725dc35799449c
Fiantso Harena <fiantsorav@gmail.com>
2024-01-19 08:47:10 +0300

    chore: create a main app redux slices

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -2,10 +2,6 @@ import { combineReducers } from "redux";
 import storage from "redux-persist/lib/storage";
 
 import appReducer from "./slices/app";
-import audioCallReducer from "./slices/audioCall";
-import videoCallReducer from "./slices/videoCall";
-import authReducer from "./slices/auth";
-import conversationReducer from "./slices/conversation";
 
 export const rootPersistConfig = {
   key: "root",
@@ -15,8 +11,4 @@ export const rootPersistConfig = {
 
 export const rootReducer = combineReducers({
   app: appReducer,
-  auth: authReducer,
-  conversation: conversationReducer,
-  audioCall: audioCallReducer,
-  videoCall: videoCallReducer,
 });
