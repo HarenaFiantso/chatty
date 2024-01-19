8f387b250c8b37b950c56b7fa37b343431659de0
Fiantso Harena <fiantsorav@gmail.com>
2024-01-19 17:58:44 +0300

    chore: reorganise the main disposition of the application

--- src/layouts/dashboard/index.jsx
+++ src/layouts/dashboard/index.jsx
@@ -1,9 +1,15 @@
+import { Stack } from "@mui/material";
 import SideNav from "./SideNav";
+import useResponsive from "../../hooks/useResponsive";
+import { Outlet } from "react-router-dom";
 
 export default function DashboardLayout() {
+  const isDesktop = useResponsive("up", "md");
+
   return (
-    <>
-      <SideNav />
-    </>
+    <Stack direction="row">
+      {isDesktop && <SideNav />}
+      <Outlet />
+    </Stack>
   );
 }
