976b7c53e541b0c5571bc1e0267a8653ed2ae978
Fiantso Harena <fiantsorav@gmail.com>
2024-01-19 20:25:34 +0300

    chore: create chatElement component

--- src/pages/dashboard/Chats.jsx
+++ src/pages/dashboard/Chats.jsx
@@ -1,12 +1,27 @@
-import { Box, IconButton, Stack, Typography, useTheme } from "@mui/material";
+import {
+  Box,
+  Button,
+  Divider,
+  IconButton,
+  Stack,
+  Typography,
+  useTheme,
+} from "@mui/material";
 import useResponsive from "../../hooks/useResponsive";
 import BottomNav from "../../layouts/dashboard/BottomNav";
-import { CircleDashed, Users } from "phosphor-react";
+import {
+  ArchiveBox,
+  CircleDashed,
+  MagnifyingGlass,
+  Users,
+} from "phosphor-react";
 import {
   Search,
   SearchIconWrapper,
   StyledInputBase,
 } from "../../components/search";
+import { SimpleBarStyle } from "../../components/Scrollbar";
+import { useSelector } from "react-redux";
 
 export default function Chats() {
   const theme = useTheme();
@@ -27,7 +42,7 @@ export default function Chats() {
         }}
       >
         {!isDesktop && <BottomNav />}
-        <Stack p={3} spacing={2} sx={{ maxHeight: "100vh" }}>
+        <Stack p={3} spacing={3} sx={{ maxHeight: "100vh" }}>
           <Stack
             alignItems="center"
             justifyContent="space-between"
@@ -47,8 +62,32 @@ export default function Chats() {
 
           <Stack sx={{ width: "100%" }}>
             <Search>
-              <SearchIconWrapper></SearchIconWrapper>
+              <SearchIconWrapper>
+                <MagnifyingGlass color="#709CE6" />
+              </SearchIconWrapper>
+              <StyledInputBase
+                placeholder="Search…"
+                inputProps={{ "aria-label": "search" }}
+              />
             </Search>
+          </Stack>
+
+          <Stack spacing={1}>
+            <Stack direction="row" spacing={1.5} alignItems="center">
+              <ArchiveBox size={24} />
+              <Button variant="text">Archive</Button>
+            </Stack>
+            <Divider />
+          </Stack>
+          <Stack sx={{ flexGrow: 1, overflow: "scroll", height: "100%" }}>
+            <SimpleBarStyle timeout={500} clickOnTrack={false}>
+              <Stack spacing={2.4}>
+                <Typography variant="subtitle2" sx={{ color: "#676667" }}>
+                  All Chats
+                </Typography>
+                <ChatElement />
+              </Stack>
+            </SimpleBarStyle>
           </Stack>
         </Stack>
       </Box>
