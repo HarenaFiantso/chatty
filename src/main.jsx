7dc63201c0c6d6dc01f7b88a9bbeea622dbb8dd9
Fiantso Harena <fiantsorav@gmail.com>
2024-01-19 09:38:46 +0300

    chore: put the redux store in the main.jsx file

--- src/main.jsx
+++ src/main.jsx
@@ -7,13 +7,13 @@ import { HelmetProvider } from "react-helmet-async";
 import { Provider as ReduxProvider } from "react-redux";
 
 import SettingsProvider from "./context/SettingsContext";
-import { store } from "./redux/store.js";
+import { store } from "./redux/store";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <HelmetProvider>
-      <ReduxProvider>
-        <SettingsProvider store={store}>
+      <ReduxProvider store={store}>
+        <SettingsProvider>
           <BrowserRouter>
             <App />
           </BrowserRouter>
